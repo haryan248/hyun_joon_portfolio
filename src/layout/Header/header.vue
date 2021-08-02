99dd8fd11cfcef4fc283df69da8f1c4dc67515b3
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-02 18:38:40 +0900

    헤더 클릭시 원하는 위치 이동

--- src/layout/Header/header.vue
+++ src/layout/Header/header.vue
@@ -4,7 +4,7 @@
             <div class="Header_content">
                 <div class="Header_title">Hyun Joon's Portfolio</div>
                 <div class="Header_navigation-menus">
-                    <div class="Header_navigation-menu" v-for="(HeaderNavMenu, index) in HeaderNavMenus" :key="index">
+                    <div @click="moveTotab(HeaderNavMenu.id)" class="Header_navigation-menu" v-for="(HeaderNavMenu, index) in HeaderNavMenus" :key="index">
                         {{ HeaderNavMenu.name }}
                     </div>
                 </div>
@@ -24,26 +24,38 @@ export default {
                 { id: 4, name: "Career" },
             ],
             showHeader: false,
+            aboutLocation: 0,
+            skillLocation: 0,
+            projectLocation: 0,
+            careerLocation: 0,
         };
     },
     computed: {},
     mounted() {
         window.addEventListener("scroll", this.handleScroll);
+        this.aboutLocation = document.querySelector(".section_myinfo").offsetTop;
+        this.skillLocation = document.querySelector(".section_myinfo").offsetTop - 72;
+        this.projectLocation = document.querySelector(".section_project").offsetTop - 72;
+        this.careerLocation = document.querySelector(".section_career").offsetTop - 72;
     },
     methods: {
         handleScroll() {
-            // if (this.showHeader) {
-            //     this.modalCurPosition = this.modalObject.scrollTop;
-            // } else {
-            //     this.curPosition = window.scrollY;
-            // }
             if (window.scrollY + 72 >= document.getElementsByClassName("banner_content")[0].offsetHeight) {
                 this.showHeader = true;
             } else {
                 this.showHeader = false;
             }
-            console.log(window.scrollY);
-            console.log();
+        },
+        moveTotab(id) {
+            if (id === 1) {
+                window.scrollTo({ top: this.aboutLocation, behavior: "smooth" });
+            } else if (id === 2) {
+                window.scrollTo({ top: this.skillLocation, behavior: "smooth" });
+            } else if (id === 3) {
+                window.scrollTo({ top: this.projectLocation, behavior: "smooth" });
+            } else if (id === 4) {
+                window.scrollTo({ top: this.careerLocation, behavior: "smooth" });
+            }
         },
     },
 };
