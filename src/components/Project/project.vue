dcc1e6fbf7d7e479e0096594fad9d86f4b15ea07
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-02 23:59:29 +0900

    프로젝트 코드 리팩토링

--- src/components/Project/project.vue
+++ src/components/Project/project.vue
@@ -18,37 +18,9 @@
                                     서비스를 이용하게 됩니다. 서비스 영역의 예시로는 강의동(건물) 출입기록, 학식 주문, 장소 대여 등이 존재합니다.
                                 </p>
                             </div>
-                            <!-- <div class="project_description" v-for="(project, index) in projects" :key="index">
-                                    <div class="projects_label">{{ project.participant.label }}</div>
-                                    <div class="projects_value">{{ project.participant.value }}</div>
-                                </div> -->
-                            <div class="project_description">
-                                <div class="projects_label">참여 인원</div>
-                                <div class="projects_value">5 명</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">개발 기간</div>
-                                <div class="projects_value">2021.04 ~ 2021.07</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">주요 기능</div>
-                                <div class="projects_value">회원가입, 회원찾기, 간편비밀번호 백업,출입 여부 확인, QR 체크인, 다크모드, 프로필 변경</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">Front-End</div>
-                                <div class="projects_value">Vue.js</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">Back-End</div>
-                                <div class="projects_value">Django REST Framework</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">Blockchain-Network</div>
-                                <div class="projects_value">Blockchain Hyperledger-Indy</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">Deployment</div>
-                                <div class="projects_value">Nginx, Naver Cloud Platform</div>
+                            <div class="project_description" v-for="(project_item, index) in projects[0]" :key="index">
+                                <div class="projects_label">{{ project_item.label }}</div>
+                                <div class="projects_value">{{ project_item.value }}</div>
                             </div>
                             <div class="project_intro_button_box">
                                 <div class="project_intro_button" onclick="youtube_on('om3f3AuP0Tc')">
@@ -72,29 +44,9 @@
                                     웹 프로그래밍 교과목에서 HTML, CSS, JavaScript 만으로 진행되었던 프로젝트에서 부족함을 느꼈고, 지능웹설계 교과목에서 정적인 웹페이지를 동적인 웹페이지로 해당 프로젝트를 발전, 확장 및 향상시켰습니다.
                                 </p>
                             </div>
-                            <div class="project_description">
-                                <div class="projects_label">참여 인원</div>
-                                <div class="projects_value">1 명</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">개발 기간</div>
-                                <div class="projects_value">2020.09 ~ 2021.12</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">주요 기능</div>
-                                <div class="projects_value">로그인, 회원가입, 회원정보 수정, 차량 등록, 차량 삭제, 게시판 등록, 게시판 삭제 등</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">Front-End</div>
-                                <div class="projects_value">JSP</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">Back-End</div>
-                                <div class="projects_value">JSP, MySQL</div>
-                            </div>
-                            <div class="project_description">
-                                <div class="projects_label">Deployment</div>
-                                <div class="projects_value">AWS(EC2)</div>
+                            <div class="project_description" v-for="(project_item, index) in projects[1]" :key="index">
+                                <div class="projects_label">{{ project_item.label }}</div>
+                                <div class="projects_value">{{ project_item.value }}</div>
                             </div>
                             <div class="project_intro_button_box">
                                 <div class="project_intro_button" onclick="youtube_on('om3f3AuP0Tc')">
@@ -116,7 +68,26 @@
 export default {
     name: "Project",
     data() {
-        return {};
+        return {
+            projects: [
+                {
+                    participant: { label: "참여 인원", value: "5 명" },
+                    duration: { label: "개발 기간", value: "21.04 ~ 21.07" },
+                    function: { label: "주요 기능", value: "회원가입, 회원찾기, 간편비밀번호 백업,출입 여부 확인, QR 체크인, 다크모드, 프로필 변경" },
+                    frontend: { label: "Front-End", value: "Vue.js" },
+                    backend: { label: "Back-End", value: "Django REST Framework" },
+                    deployment: { label: "Deployment", value: "Nginx, Naver Cloud Platform" },
+                },
+                {
+                    participant: { label: "참여 인원", value: "1 명" },
+                    duration: { label: "개발 기간", value: "2020.09 ~ 2021.12" },
+                    function: { label: "주요 기능", value: "로그인, 회원가입, 회원정보 수정, 차량 등록, 차량 삭제, 게시판 등록, 게시판 삭제 등" },
+                    frontend: { label: "Front-End", value: "JSP" },
+                    backend: { label: "Back-End", value: "JSP, MySQL" },
+                    deployment: { label: "Deployment", value: "AWS(EC2)" },
+                },
+            ],
+        };
     },
 };
 </script>
