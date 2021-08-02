0e8a331ea2e9ac20ed333fbd143af03f1496a32d
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-02 23:57:35 +0900

    스크롤 애니메이션 및 데이터 수정

--- src/pages/Main-content/maincontent.vue
+++ src/pages/Main-content/maincontent.vue
@@ -2,7 +2,7 @@
     <div>
         <Banner />
         <!-- about me -->
-        <About />
+        <About :scrollHeight="scrollHeight" />
         <!-- skill -->
         <Skill />
         <!-- projects -->
@@ -22,18 +22,9 @@ import Career from "../../components/Career/career.vue";
 export default {
     name: "Maincontent",
     components: { Projects, About, Career, Banner, Skill },
+    props: { scrollHeight: Number },
     data() {
-        return {
-            projects: [
-                {
-                    participant: { label: "참여 인원", value: "5 명" },
-                    duration: { label: "개발 기간", value: "21.04 ~ 21.07" },
-                    function: { label: "주요 기능", value: "회원가입, 회원찾기, 간편비밀번호 백업,출입 여부 확인, QR 체크인, 다크모드, 프로필 변경" },
-                    frontend: { label: "Front-End", value: "Vue.js" },
-                    backend: { label: "Back-End", value: "Django REST Framework" },
-                },
-            ],
-        };
+        return {};
     },
 };
 </script>
