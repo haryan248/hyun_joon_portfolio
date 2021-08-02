d6e34b5e87c4458125f47cb46e522da31c9c041c
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-02 18:20:41 +0900

    푸터 레이아웃 설정

---
+++ src/layout/Footer/footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <footer class="Footer_Footer">
+            <div class="Footer_icons">
+                <div class="Footer_icon-wrapper Footer_bg-white" title="GitHub"><img class="Footer_icon__Rb30k" src="/images/footer/git-hub.png" alt="" /></div>
+                <div class="Footer_icon-wrapper Footer_bg-white" title="Tistory"><img class="Footer_icon__Rb30k" src="/images/footer/tistory.png" alt="" /></div>
+                <div class="Footer_icon-wrapper Footer_bg-white" title="Instagram"><img class="Footer_icon__Rb30k" src="/images/footer/instagram.png" alt="" /></div>
+                <div class="Footer_icon-wrapper Footer_bg-white" title="Facebook"><img class="Footer_icon__Rb30k" src="/images/footer/facebook.png" alt="" /></div>
+            </div>
+            <div class="Footer_copyright__16dqg">© 2021. Ha Hyun Joon. All rights reserved.</div>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    name: "Footer",
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style scoped>
+@import "./footer.css";
+</style>
