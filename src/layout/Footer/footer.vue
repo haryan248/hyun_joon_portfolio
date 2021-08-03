1fbe2568a8d0c169e5e61238cccde0d358ae90d2
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-03 19:21:13 +0900

    푸터 이미지 및 레이아웃 구현

--- src/layout/Footer/footer.vue
+++ src/layout/Footer/footer.vue
@@ -2,12 +2,17 @@
     <div>
         <footer class="Footer_Footer">
             <div class="Footer_icons">
-                <div class="Footer_icon-wrapper Footer_bg-white" title="GitHub"><img class="Footer_icon__Rb30k" src="/images/footer/git-hub.png" alt="" /></div>
-                <div class="Footer_icon-wrapper Footer_bg-white" title="Tistory"><img class="Footer_icon__Rb30k" src="/images/footer/tistory.png" alt="" /></div>
-                <div class="Footer_icon-wrapper Footer_bg-white" title="Instagram"><img class="Footer_icon__Rb30k" src="/images/footer/instagram.png" alt="" /></div>
-                <div class="Footer_icon-wrapper Footer_bg-white" title="Facebook"><img class="Footer_icon__Rb30k" src="/images/footer/facebook.png" alt="" /></div>
+                <a href="https://github.com/haryan248" target="_blank">
+                    <div class="Footer_icon-wrapper Footer_bg-white" title="GitHub"><img class="Footer_icon" src="../../assets/images/footer/github.png" /></div>
+                </a>
+                <a href="https://www.instagram.com/h__.__joon/" target="_blank">
+                    <div class="Footer_icon-wrapper Footer_bg-white" title="Instagram"><img class="Footer_icon" src="../../assets/images/footer/instagram.png" /></div>
+                </a>
+                <a href="mailto:haryan96@gmail.com">
+                    <div class="Footer_icon-wrapper Footer_bg-white" title="Mail"><img class="Footer_icon" src="../../assets/images/footer/email.png" /></div>
+                </a>
             </div>
-            <div class="Footer_copyright__16dqg">© 2021. Ha Hyun Joon. All rights reserved.</div>
+            <div class="Footer_copyright">© 2021. Ha Hyun Joon. All rights reserved.</div>
         </footer>
     </div>
 </template>
