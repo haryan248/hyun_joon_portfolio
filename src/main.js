53002d1ff7723407f898b47c9e1a0456ea7c2c06
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-04 17:42:11 +0900

    스와이퍼 패키지 추가

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueAwesomeSwiper from "vue-awesome-swiper";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
+
+Vue.use(VueAwesomeSwiper);
