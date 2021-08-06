e50c95f99dc7f0da909d4c2c55dbcb47486b3bc9
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-06 18:50:49 +0900

    배너 디자인 추가

--- src/components/Banner/banner.vue
+++ src/components/Banner/banner.vue
@@ -1,8 +1,22 @@
 <template>
     <div class="section_banner">
         <div class="banner_content">
-            <div class="banner_title">WELCOME!<br />HYUNJOON'S PORTFOLIO</div>
+            <div class="banner_title">
+                <div class="banner_animaiton-left">WELCOME!</div>
+                <div class="banner_animaiton-right">HYUNJOON'S PORTFOLIO</div>
+            </div>
             <div class="banner_more-button" @click="clickMoreButton"></div>
+            <div class="circle_wrapper">
+                <div class="circle circle_1"></div>
+                <div class="circle circle_2"></div>
+                <div class="circle circle_3"></div>
+                <div class="circle circle_4"></div>
+                <div class="circle circle_5"></div>
+                <div class="circle circle_6"></div>
+                <div class="circle circle_7"></div>
+                <div class="circle circle_8"></div>
+                <div class="circle circle_9"></div>
+            </div>
         </div>
     </div>
 </template>
