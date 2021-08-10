c5949768f3a2999c08b4eddb875e0de50a2a81ad
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-10 19:51:19 +0900

    스킬 툴팁 추가

--- src/components/Skill/skill.vue
+++ src/components/Skill/skill.vue
@@ -5,6 +5,29 @@
                 <span class="section_title border_bottom">SKILLS</span>
             </div>
         </div>
+        <div class="skill_info_container" :class="computedClass">
+            <div class="skill_info_wrapper">
+                <div class="loader">
+                    <div class="loaderBar"></div>
+                </div>
+                <div class="skill_info">
+                    <p class="skill_info_content">
+                        &nbsp;&nbsp;&#183; 입문 &#183; 초급 &#183; 중급 &#183; 고급 &#183;
+                        <br />
+                        &nbsp;0 &nbsp; - &nbsp;25 &nbsp;- &nbsp;50 &nbsp;-&nbsp; 75 &nbsp;-&nbsp;100
+                        <span class="skill_tooltip">
+                            <p>입문: 한번 경험해본 기술 스택</p>
+                            <br />
+                            <p>초급: 코드를 보고 디버깅이 가능</p>
+                            <br />
+                            <p>중급: 코드를 이해하며 어느정도 학습됌</p>
+                            <br />
+                            <p>고급: 코드를 응용해서 구현에 익숙함</p>
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
         <div class="skill_container" :class="[{ 'stagger-item': checkHeight }, { 'stagger-item-out': !checkHeight }]" v-for="(skill, index) in skills" :key="index">
             <div class="status_title ">{{ index }}</div>
             <div class="status_content" v-for="skill_item in skill" :key="skill_item.label">
@@ -35,12 +58,17 @@ export default {
                 BackEnd: [
                     { id: 5, label: "Django", value: "50" },
                     { id: 6, label: "Spring", value: "20" },
+                    { id: 7, label: "MySQL", value: "30" },
+                ],
+                Deployment: [
+                    { id: 8, label: "AWS", value: "50" },
+                    { id: 9, label: "NCP", value: "50" },
                 ],
                 ConfigurationManagement: [{ id: 7, label: "GitHub", value: "70" }],
                 Communication: [
-                    { id: 8, label: "Slack", value: "80" },
-                    { id: 9, label: "Figma", value: "80" },
-                    { id: 10, label: "Jira", value: "70" },
+                    { id: 10, label: "Slack", value: "80" },
+                    { id: 11, label: "Figma", value: "80" },
+                    { id: 12, label: "Jira", value: "70" },
                 ],
             },
         };
@@ -70,7 +98,7 @@ export default {
         },
         setAnimationStyle() {
             let duration = 0.7;
-            for (let i = 0; i < this.skills.FrontEnd.length + this.skills.BackEnd.length + this.skills.ConfigurationManagement.length + this.skills.Communication.length; i++) {
+            for (let i = 0; i < this.skills.FrontEnd.length + this.skills.BackEnd.length + this.skills.Deployment.length + this.skills.ConfigurationManagement.length + this.skills.Communication.length; i++) {
                 duration += 0.3;
                 this.durationList.push(duration);
             }
